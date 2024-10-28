3ccc05823d779eb4b93e696551356ae5c87a0635
abdullatif <abdullatifjr002@gmail.com>
2024-10-29 00:08:28 +0300

    upload files

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'; 
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/redux-react/",
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+      ],
+    },
+  },
+})
